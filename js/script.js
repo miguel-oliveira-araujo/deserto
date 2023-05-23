7e33e62a5b3a385b5d3ccaade21374bf0b78bdad
miguel-oliveira-araujo <migueloliveira.araujo@etec.sp.gov.br>
2023-05-23 18:16:35 -0300

    atualização

---
+++ js/script.js
@@ -0,0 +1,344 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+    ctx.fillStyle ='#9e9b70';
+    ctx.fillRect(1,500,2000,300);
+
+    
+    ctx.fillStyle='yellow';
+    ctx.beginPath();
+    ctx.arc(750,200,80,0,Math.PI*2)
+    ctx.fill();
+
+    ctx.fillStyle='#944913';
+    ctx.beginPath();
+    ctx.arc(750,200,80,0,Math.PI*1)
+    ctx.fill();
+
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1,750);
+    ctx.lineTo(80,750);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(120,750);
+    ctx.lineTo(220,750);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(275,750);
+    ctx.lineTo(375,750);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(450,750);
+    ctx.lineTo(550,750);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(550,750);
+    ctx.lineTo(575,750);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(600,750);
+    ctx.lineTo(700,750);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(750,750);
+    ctx.lineTo(850,750);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(900,750);
+    ctx.lineTo(1000,750);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1050,750);
+    ctx.lineTo(1150,750);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1250,750);
+    ctx.lineTo(1350,750);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';//começo da segunda linha
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(10,700);
+    ctx.lineTo(100,700);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(150,700);
+    ctx.lineTo(250,700);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(300,700);
+    ctx.lineTo(400,700);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(450,700);
+    ctx.lineTo(550,700);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(600,700);
+    ctx.lineTo(700,700);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(750,700);
+    ctx.lineTo(800,700);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(850,700);
+    ctx.lineTo(950,700);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1000,700);
+    ctx.lineTo(1150,700);
+    ctx.stroke();
+
+     ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1200,700);
+    ctx.lineTo(1300,700);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1250,700);
+    ctx.lineTo(1350,700);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';//começo da terceira linha
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(10,650);
+    ctx.lineTo(100,650);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(150,650);
+    ctx.lineTo(250,650);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(300,650);
+    ctx.lineTo(400,650);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(450,650);
+    ctx.lineTo(550,650);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(600,650);
+    ctx.lineTo(700,650);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(750,650);
+    ctx.lineTo(800,650);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(850,650);
+    ctx.lineTo(950,650);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1000,650);
+    ctx.lineTo(1150,650);
+    ctx.stroke();
+
+     ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1200,650);
+    ctx.lineTo(1300,650);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1250,650);
+    ctx.lineTo(1350,650);
+    ctx.stroke();//fim da terceira linha
+
+    ctx.strokeStyle='black';//começo da 4 linha
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(10,600);
+    ctx.lineTo(100,600);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(150,600);
+    ctx.lineTo(250,600);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(300,600);
+    ctx.lineTo(400,600);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(450,600);
+    ctx.lineTo(550,600);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(600,600);
+    ctx.lineTo(700,600);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(750,600);
+    ctx.lineTo(800,600);
+    ctx.stroke();
+
+    
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(850,600);
+    ctx.lineTo(950,600);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1000,600);
+    ctx.lineTo(1150,600);
+    ctx.stroke();
+
+     ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1200,600);
+    ctx.lineTo(1300,600);
+    ctx.stroke();
+
+    ctx.strokeStyle='black';
+    ctx.lineWidth='5';
+    ctx.beginPath();
+    ctx.moveTo(1250,600);
+    ctx.lineTo(1350,600);
+    ctx.stroke();
+
+    var imagem = new Image();
+imagem.onload=function(){
+ctx.drawImage(this, 10, 350, 200, 200);}
+imagem.src = 'img/cacto.png';
+
+    var imagem = new Image();
+imagem.onload=function(){
+ctx.drawImage(this, 400, 320, 200, 200);}
+imagem.src = 'img/cacto2.png';
+
+var imagem = new Image();
+imagem.onload=function(){
+ctx.drawImage(this, 900, 320, 200, 200);}
+imagem.src = 'img/cacto3.png';
+
+ctx.strokeStyle='black';//cacto
+ctx.lineWidth='30';
+ctx.beginPath();
+ctx.moveTo(1200,530);
+ctx.lineTo(1200,300);
+ctx.stroke();
+
+ctx.strokeStyle='black';//cacto
+ctx.lineWidth='30';
+ctx.beginPath();
+ctx.moveTo(1250,400);
+ctx.lineTo(1150,400);
+ctx.stroke();
+
+ctx.strokeStyle='black';//cacto
+ctx.lineWidth='30';
+ctx.beginPath();
+ctx.moveTo(1250,350);
+ctx.lineTo(1250,415);
+ctx.stroke();
